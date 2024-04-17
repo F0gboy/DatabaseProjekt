cfccf99197b429a46d5a020a24caeffcdbb46606
F0gboy <Jasp3r@live.dk>
2024-04-17 10:54:07 +0200

    base commit

---
+++ DatabaseProjekt/Program.cs
@@ -0,0 +1,16 @@
+﻿using Npgsql;
+using NpgsqlTypes;
+
+namespace DatabaseProjekt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = "Host=localhost;Username=postgres;Password=xxxxx;Database=data";
+
+            NpgsqlDataSource dataSource = NpgsqlDataSource.Create(connectionString);
+
+        }
+    }
+}
