f21b81ee4143ccfb55bd0641507870d37a948820
Marc Larsen <57370@student.eadania.dk>
2024-04-18 13:09:29 +0200

    bedre

--- DatabaseProjekt/Program.cs
+++ DatabaseProjekt/Program.cs
@@ -17,7 +17,7 @@ namespace DatabaseProjekt
             LoginSystem loginSystem = new LoginSystem(dataSource);
             
 
-            string createTableSql = "CREATE TABLE IF NOT EXISTS Characters (Charater_id integer NOT NULL GENERATED ALWAYS AS IDENTITY(INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1), Login_id integer NOT NULL, LvL integer NOT NULL, Death_Order integer NOT NULL, Class character varying(50) NOT NULL, Charater_names character varying(50) NOT NULL, Stege integer NOT NULL, Kills integer NOT NULL, Death character varying(50) NOT NULL, PRIMARY KEY(Charater_id), CONSTRAINT fk_login FOREIGN KEY (login_id) REFERENCES login(login_id))";
+            string createTableSql = "CREATE TABLE IF NOT EXISTS Characters (Character_id integer NOT NULL GENERATED ALWAYS AS IDENTITY(INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1), Login_id integer NOT NULL, LvL integer NOT NULL, Death_Order integer NOT NULL, Class character varying(50) NOT NULL, Character_names character varying(50) NOT NULL, Stege integer NOT NULL, Kills integer NOT NULL, Death character varying(50) NOT NULL, PRIMARY KEY(Character_id), CONSTRAINT fk_login FOREIGN KEY (login_id) REFERENCES login(login_id))";
 
             try
             {
